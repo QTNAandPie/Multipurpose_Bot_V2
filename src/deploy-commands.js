f9b1e3b21619721515684befbf4f017d2028ca33
QTNA <quan7colorful@outlook.com>
2024-06-05 21:26:10 +0700

    Add deploy-commands

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,10 +1,10 @@
-require('dotenv/config')
+require("dotenv/config");
 
-const { REST, Routes } = require('discord.js');
-const { clientId } = require('./config.json');
+const { REST, Routes } = require("discord.js");
+const { clientId } = require("./../config.json");
 
 const rest = new REST().setToken(process.env.TOKEN);
 
-rest.delete(Routes.applicationCommand(clientId, 'commandId'))
-	.then(() => console.log('Successfully deleted application command'))
-	.catch(console.error);
+rest.delete(Routes.applicationCommand(clientId, "commandId"))
+    .then(() => console.log("Successfully deleted application command"))
+    .catch(console.error);
