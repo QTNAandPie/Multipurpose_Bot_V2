4d290de4ea103d20b0979a00fbf545d6f789f4a4
QTNA <quan7colorful@outlook.com>
2024-06-08 20:05:46 +0700

    Gift code premium generate schema

---
+++ src/schemas/code-gen.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const codeSchema = new Schema({
+    code: {
+        type: "String",
+        default: null
+    },
+    expiresAt: {
+        type: Number,
+        default: null
+    },
+    plan: {
+        type: String,
+        default: null
+    }
+});
+
+module.exports = model("Code", codeSchema);
