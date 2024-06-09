8b3a5a0f96606d7760c5425e52f11456f9e517c3
QTNA <quan7colorful@outlook.com>
2024-06-09 15:20:53 +0700

    feat:add premium track event

---
+++ src/events/messageCreate/premiumTracks.js
@@ -0,0 +1,32 @@
+const { Client, Message } = require("discord.js");
+const Premium = require("../../schemas/premium-user");
+
+/**
+ *
+ * @param {Client} client
+ * @param {Message} message
+ */
+
+module.exports = async (message, client) => {
+    const userPremium = await Premium.find({ isPremium: true });
+
+    try {
+        if (!userPremium.length) return;
+
+        userPremium.forEach(async (premium) => {
+            if (Date.now() >= premium.expiresAt) {
+                premium.isPremium = false;
+                premium.redeemedBy = [];
+                premium.redeemedAt = null;
+                premium.expiresAt = null;
+                premium.plan = null;
+
+                await premium.save();
+
+                message.channel.send("Your premium have been expired");
+            }
+        });
+    } catch (error) {
+        console.log("Failed to tracks premium user\n" + error);
+    }
+};
